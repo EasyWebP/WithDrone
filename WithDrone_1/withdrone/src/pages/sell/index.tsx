0ce18ae1f99c909564d572af2f673478d14bfc6b
younsoolim <yslim815@gmail.com>
2023-04-07 04:27:03 +0900

    refactor: px to rem

--- WithDrone_1/withdrone/src/pages/sell/index.tsx
+++ WithDrone_1/withdrone/src/pages/sell/index.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
-import styled from "styled-components";
-import { useNavigate, useParams } from "react-router-dom";
-import Commercial from "./Commercial";
-import Educational from "./Educational";
-import Filming from "./Filming";
-import Hobby from "./Hobby";
+import { useState } from 'react';
+import styled from 'styled-components';
+import { useNavigate, useParams } from 'react-router-dom';
+import Commercial from './Commercial';
+import Educational from './Educational';
+import Filming from './Filming';
+import Hobby from './Hobby';
 
 const Containers = styled.div`
   width: 100%;
@@ -12,7 +12,7 @@ const Containers = styled.div`
 `;
 
 const TabMenu = styled.ul`
-  width: 70%;
+  width: 60%;
   background-color: #ffffff;
   color: rgb(232, 234, 237);
   font-weight: bold;
@@ -20,18 +20,18 @@ const TabMenu = styled.ul`
   flex-direction: row;
   align-items: center;
   list-style: none;
-  border-radius: 10px 10px 0px 0px;
+  border-radius: 0.625rem 0.625rem 0rem 0rem;
   margin-bottom: 7rem;
-  margin-top: 100px;
-  margin-left: 100px;
+  margin-top: 6.25rem;
+  margin-left: 10rem;
 
   .submenu {
     display: flex;
     width: calc(100% / 4);
-    padding: 10px;
-    font-size: 30px;
+    padding: 0.625rem;
+    font-size: 1.875rem;
     transition: 0.5s;
-    border-radius: 10px 10px 0px 0px;
+    border-radius: 0.625rem 0.625rem 0rem 0rem;
   }
 
   .focused {
@@ -46,10 +46,10 @@ export default function Sell() {
   const [selectedTab, setSelectedTab] = useState(1);
 
   const tabs = [
-    { id: 1, label: "상업용", content: <Commercial /> },
-    { id: 2, label: "교육용", content: <Educational /> },
-    { id: 3, label: "촬영용", content: <Filming /> },
-    { id: 4, label: "취미용", content: <Hobby /> },
+    { id: 1, label: '상업용', content: <Commercial /> },
+    { id: 2, label: '교육용', content: <Educational /> },
+    { id: 3, label: '촬영용', content: <Filming /> },
+    { id: 4, label: '취미용', content: <Hobby /> },
   ];
 
   return (
@@ -57,7 +57,7 @@ export default function Sell() {
       <TabMenu>
         {tabs.map((tab) => (
           <li
-            className={tab.id === selectedTab ? "submenu focused" : "submenu"}
+            className={tab.id === selectedTab ? 'submenu focused' : 'submenu'}
             key={tab.id}
             onClick={() => setSelectedTab(tab.id)}
           >
