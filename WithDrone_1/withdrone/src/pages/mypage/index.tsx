3300cdb557454250a483e703ed3be62ad21e1a74
JoSooha <dlrhdcjs@naver.com>
2023-05-17 17:16:15 +0900

    feat: WelcomeContainer 완료

--- WithDrone_1/withdrone/src/pages/mypage/index.tsx
+++ WithDrone_1/withdrone/src/pages/mypage/index.tsx
@@ -9,13 +9,72 @@ const Containers = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
-  position: relative;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
   background-color: white;
 `;
 
+const WelcomeContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    background-color: #0F67EC;
+    opacity: 0.77;
+    height: 23.75rem;
+`
+
+const LeftContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    height: 100%;
+    justify-content: center;
+    align-items: flex-start;
+`
+
+const WelcomeContext = styled.p`
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 2.5rem;
+    line-height: 6rem;
+    color: white;
+    margin-left: 3rem;
+`
+
+const RightContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-start;
+  width: 50%;
+  height: 100%;
+`;
+
+const LogoutButton = styled.button`
+  background-color: red;
+  color: white;
+  border: none;
+  padding: 10px;
+  font-weight: bold;
+  cursor: pointer;
+  margin-top: 2rem;
+  margin-right: 2rem;
+  `;
+
 export default function Mypage() {
-  return <Containers>mypage</Containers>;
+  return (
+    <Containers>
+        <WelcomeContainer>
+            <LeftContainer>
+                <WelcomeContext>사가정변태남</WelcomeContext>
+                <WelcomeContext>임윤수님의 이번 달 쇼핑 목록입니다</WelcomeContext>
+                <WelcomeContext>limyoonsoo@naver.com</WelcomeContext>
+            </LeftContainer>
+            <RightContainer>
+                <LogoutButton>로그아웃</LogoutButton>
+            </RightContainer>
+        </WelcomeContainer>
+    </Containers>
+  )
 }
