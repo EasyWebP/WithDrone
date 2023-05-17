62f7649e645190f20e0c8b11a52446a77871ba19
miin-hyukkk <ham9893@naver.com>
2023-05-17 19:06:08 +0900

    feat: useValidate 훅 생성

--- WithDrone_1/withdrone/src/hooks/useValidate.ts
+++ WithDrone_1/withdrone/src/hooks/useValidate.ts
@@ -1,1 +1,28 @@
-export {};
+import { useEffect, useState } from "react";
+
+import useInput from "./useInput";
+
+interface ValidateProps {
+  validator: Function;
+  initState?: string;
+}
+const useValidate = ({ validator, initState = "" }: ValidateProps) => {
+  const [value, onChangeValue, setValue] = useInput(initState);
+  const [isValidateValue, setIsValidateValue] = useState(false);
+
+  useEffect(() => {
+    if (value) {
+      setIsValidateValue(validator(value));
+    }
+  }, [value, validator]);
+
+  return [
+    value,
+    onChangeValue,
+    setValue,
+    isValidateValue,
+    setIsValidateValue,
+  ] as const;
+};
+
+export default useValidate;
