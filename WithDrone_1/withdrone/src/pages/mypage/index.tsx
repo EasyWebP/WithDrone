8500ebe6414f96e6a4eaba8695294cfe791375e3
JoSooha <dlrhdcjs@naver.com>
2023-05-19 23:57:16 +0900

    feat: OrderList 컴포넌트화

--- WithDrone_1/withdrone/src/pages/mypage/index.tsx
+++ WithDrone_1/withdrone/src/pages/mypage/index.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import COLORS from "../../constants/color";
 import * as S from "../../components/Layout/Header/index.styles";
 import logo from "../../img/drone.png";
-import mavic2 from "../../img/mavic2.jpg";
 import { fetchMember } from "../../api/user";
+import OrderList from "./OrderList";
+import LikeList from "./LikeList";
+import CartList from "./CartList";
 
 
 const Containers = styled.div`
@@ -96,78 +98,6 @@ const TableContainer = styled.div`
   height: auto;
 `;
 
-const Table = styled.table`
-  width: 100%;
-  border-collapse: collapse;
-  border: 
-`;
-
-const ColumnGroup = styled.colgroup``;
-
-const Column = styled.col`
-  width: ${props => props.width};
-`;
-
-const TableHead = styled.thead`
-  background-color: white;
-`;
-
-const TableHeadRow = styled.tr`
-`;
-
-const TableHeadCell = styled.th`
-  padding: 1rem;
-  text-align: center;
-  font-weight: bold;
-  border-top: 0.1rem solid black;
-`;
-
-const TableBody = styled.tbody`
-    border-bottom: 0.1rem solid black;
-`;
-
-const TableBodyRow = styled.tr`
-
-`;
-
-const TableBodyCell = styled.td`
-  padding: 10px;
-  text-align: center;
-`;
-
-const TableBodyFirst = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: center;
-`; 
-
-const TableImg = styled.img`
-    alt: "Product";
-    width: 20rem;
-    heigh: 20rem;
-`
-
-const TableInfoContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-`
-
-const TableInfoContent = styled.p`
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 700;
-    text-align: left;
-`
-
-const TotalContext = styled.p`
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 700;
-    align-self: flex-end;
-    margin: 5rem 10rem;
-`
-
 export default function Mypage() {
     const [member, setMember] = useState({username: '김동준', nickname: '김도옹준', email: 'dongjunkim99@icloud.com'}); // 초기값 없으면 비동기라 null 될수있다 에러뜸
 
@@ -178,6 +108,15 @@ export default function Mypage() {
         setMember(fetchedData); // 데이터 상태 업데이트
       });
   }, []);
+
+  const { state } = useLocation();
+  const [selectedTabs, setSelectedTabs] = useState(Number(state) || 1);
+
+  const tabs = [
+    { id: 1, label: "주문 내역 조회", content: <OrderList /> },
+    { id: 2, label: "찜 목록", content: <LikeList /> },
+    { id: 3, label: "장바구니", content: <CartList /> },
+  ];
  
   return (
     <Containers>
@@ -200,42 +139,10 @@ export default function Mypage() {
                     <TabMenuItem style={{color: "#D9D9D9"}}>장바구니</TabMenuItem>
                 </TabMenu>
             </TabContainer>
-            <Table>
-                <ColumnGroup>
-                    <Column width="40%" />
-                    <Column width="20%" />
-                    <Column width="20%" />
-                    <Column width="20%" />
-                </ColumnGroup>
-                <TableHead>
-                    <TableHeadRow>
-                        <TableHeadCell>상품 정보</TableHeadCell>
-                        <TableHeadCell>주문일자</TableHeadCell>
-                        <TableHeadCell>주문번호</TableHeadCell>
-                        <TableHeadCell>수량</TableHeadCell>
-                    </TableHeadRow>
-                </TableHead>
-                <TableBody>
-                    <TableBodyRow>
-                        <TableBodyCell>
-                            <TableBodyFirst>
-                                <TableImg src={mavic2}/> 
-                                <TableInfoContainer>
-                                    <TableInfoContent>Matrice M600 Matrice M600</TableInfoContent>
-                                    <TableInfoContent>20,000원</TableInfoContent>
-                                    <TableInfoContent>삼성</TableInfoContent>
-                                </TableInfoContainer>
-                            </TableBodyFirst>
-                        </TableBodyCell>
-                        <TableBodyCell>2023-05-17</TableBodyCell>
-                        <TableBodyCell>12345678</TableBodyCell>
-                        <TableBodyCell>1개</TableBodyCell>
-                    </TableBodyRow>
-                    
-                </TableBody>
-            </Table>
+            <OrderList></OrderList>
+            
         </TableContainer>
-        <TotalContext>총 800,000원</TotalContext>
+        
     </Containers>
   )
 }
