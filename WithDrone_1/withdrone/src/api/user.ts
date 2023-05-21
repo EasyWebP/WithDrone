6460f462a1f43698da58a045180429b9aba2b3cd
miin-hyukkk <ham9893@naver.com>
2023-05-21 19:11:54 +0900

    feat: 내정보조회 api 등록

--- WithDrone_1/withdrone/src/api/user.ts
+++ WithDrone_1/withdrone/src/api/user.ts
@@ -2,13 +2,16 @@ import { authorizationClient, unAuthorizationClient } from ".";
 import API from "./config";
 
 export const fetchMember = async () => {
-  const response = await authorizationClient.get(
-    `${API.MEMBER}`
-  )
+  const response = await authorizationClient.get(`${API.MEMBER}`);
   return response.data;
 };
 
 export const fetchPost = async () => {
-    const response = await (await fetch(API.MEMBER)).json();
-    return response;
-  };
+  const response = await (await fetch(API.MEMBER)).json();
+  return response;
+};
+
+export const loadMe = async () => {
+  const { data } = await authorizationClient.get(API.MEMBER);
+  return data;
+};
