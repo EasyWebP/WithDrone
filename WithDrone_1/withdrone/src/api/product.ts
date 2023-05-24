19c253bfb50358f110bb83cf98af23fd2b1408a6
miin-hyukkk <ham9893@naver.com>
2023-05-24 10:27:06 +0900

    feat: 장바구니 api 구현

--- WithDrone_1/withdrone/src/api/product.ts
+++ WithDrone_1/withdrone/src/api/product.ts
@@ -9,8 +9,16 @@ export const fetchProductList = async (like: boolean, price: boolean) => {
 };
 
 export const fetchProduct = async (id: number) => {
-  const response = await unAuthorizationClient.get(
-    `${API.PRODUCT}/${id}`
-  );
-  return response.data;
+  const { data } = await authorizationClient.get(`${API.PRODUCT}/${id}`);
+  return data;
+};
+
+export const getLike = async (productId: object) => {
+  const { data } = await authorizationClient.post(`${API.LIKE}`, productId);
+  return data;
+};
+
+export const addCart = async (product: object) => {
+  const { data } = await authorizationClient.post(`${API.CART}`, product);
+  return data;
 };
