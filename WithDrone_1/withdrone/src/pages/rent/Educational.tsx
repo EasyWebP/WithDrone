b944a0a85c6f655dbf954b8b94e8054199058744
JoSooha <dlrhdcjs@naver.com>
2023-05-25 16:32:46 +0900

    feat: 대여 페이지 구현 완료

--- WithDrone_1/withdrone/src/pages/rent/Educational.tsx
+++ WithDrone_1/withdrone/src/pages/rent/Educational.tsx
@@ -1,10 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import * as P from "../../components/Product";
 import { fetchProductList } from "../../api/product";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Pagination from "react-js-pagination";
 import PAGENUM from "../../constants/PageNumber";
+import PATH from "../../constants/path";
 
 interface Product {
   id: number;
@@ -17,6 +18,7 @@ interface Product {
 export default function Educational() {
   const [totalProduct, setTotalProduct] = useState(0);
   const [activePage, setActivePage] = useState<number>(1);
+  const navigate = useNavigate();
 
   // @ts-ignore
   const like = useSelector((state) => state.likeReducer.like);
@@ -33,53 +35,55 @@ export default function Educational() {
   const [droneLists, setDroneLists] = useState<Product[]>([]);
 
   useEffect(() => {
-    fetchProductList("교육용", undefined).then(
-      (fetchedData) => {
-        setTotalProduct(fetchedData.numberOfElements); //해당 카테고리 개수 할당 위함
-      }
-    );
+    fetchProductList("교육용", undefined, undefined, "RENTAL").then((fetchedData) => {
+      setTotalProduct(fetchedData.numberOfElements); //해당 카테고리 개수 할당 위함
+    });
   }, []);
 
   useEffect(() => {
-    fetchProductList("교육용", likeState, priceState, undefined, {page:activePage-1,size:PAGENUM}).then(
-      (fetchedData) => {
-        setDroneLists(fetchedData.content);
-      }
-    );
+    fetchProductList("교육용", likeState, priceState, "RENTAL", {
+      page: activePage - 1,
+      size: PAGENUM,
+    }).then((fetchedData) => {
+      setDroneLists(fetchedData.content);
+    });
   }, [like, price]);
 
   const handlePageChange = (pageNumber: number) => {
     setActivePage(pageNumber);
-    fetchProductList("교육용", likeState, priceState, undefined, {page:pageNumber-1,size:PAGENUM}).then(
-      (fetchedData) => {
-        setDroneLists(fetchedData.content);
-      }
-    );
+    fetchProductList("교육용", likeState, priceState, "RENTAL", {
+      page: pageNumber - 1,
+      size: PAGENUM,
+    }).then((fetchedData) => {
+      setDroneLists(fetchedData.content);
+    });
   };
 
   return (
     <P.Containers>
       <P.ProductContainer>
         {droneLists.map((product: Product, index) => (
-          <Link to={`/detail/${product.id}`}>
-            <P.Product>
-              <P.ProductImgDiv>
-                <P.ProductImg src={product.imagePath} alt={product.name} />
-              </P.ProductImgDiv>
-              <P.ProductTitle>{product.name}</P.ProductTitle>
-              <P.ProductPrice>{product.price}</P.ProductPrice>
-            </P.Product>
-          </Link>
+          <P.Product
+            onClick={() => {
+              navigate(PATH.DETAIL, { state: product.id });
+            }}
+          >
+            <P.ProductImgDiv>
+              <P.ProductImg src={product.imagePath} alt={product.name} />
+            </P.ProductImgDiv>
+            <P.ProductTitle>{product.name}</P.ProductTitle>
+            <P.ProductPrice>{product.price}</P.ProductPrice>
+          </P.Product>
         ))}
       </P.ProductContainer>
       <P.PageNumberContainer>
-          <Pagination
-            activePage={activePage}
-            itemsCountPerPage={PAGENUM} // 페이지당 아이템 개수
-            totalItemsCount={totalProduct} // 전체 아이템 개수
-            pageRangeDisplayed={Math.ceil(totalProduct/PAGENUM)} // 표시할 페이지 범위의 개수
-            onChange={handlePageChange} // 페이지 변경 이벤트 핸들러
-          />
+        <Pagination
+          activePage={activePage}
+          itemsCountPerPage={PAGENUM} // 페이지당 아이템 개수
+          totalItemsCount={totalProduct} // 전체 아이템 개수
+          pageRangeDisplayed={Math.ceil(totalProduct / PAGENUM)} // 표시할 페이지 범위의 개수
+          onChange={handlePageChange} // 페이지 변경 이벤트 핸들러
+        />
       </P.PageNumberContainer>
     </P.Containers>
   );
